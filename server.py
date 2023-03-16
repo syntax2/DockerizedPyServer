d5c5935f5b6527e31d80b6e27e647e550f696733
Ashish Kadian <38347057+syntax2@users.noreply.github.com>
2023-03-16 16:26:03 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,13 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'Hello, World!')
+
+if __name__ == '__main__':
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    httpd.serve_forever()
